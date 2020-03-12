95832d73484951fa14f872928b3749c12f379cef
Aubrey Snider <aubrey.snider@gmail.com>
2020-03-12 02:16:35 -0500

    major update

--- js/exercise3.js
+++ js/exercise3.js
@@ -1,7 +1,7 @@
 // Make the box blue when you click the button. (Here's some code to help you get started)
 
-var button = document.getElementById("myButton");
+var button = document.getElementById("green-box-btn");
 
 button.addEventListener('click', function(){
-    
+
 })
